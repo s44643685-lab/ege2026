b3326471ac881f7fca136dae943b1946d6d09464
sonya <s44643685@gmail.com>
2026-03-14 10:40:35 +0200

    start progect

---
+++ 2.py
@@ -0,0 +1,16 @@
+# ? получение данных
+# name = input("Введите имя:")
+# print(f"Вас зовут{name}")
+
+# ? int - переводит в число
+num1 = input()
+num1 = int(num1)
+num2 = int(input())
+
+print(num1 + num2)
+
+# ? str переводит в строки
+num1 = str(5)
+num2 = str(10)
+
+print(num1 + num2)

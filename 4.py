aef74abf1d82645063a9aa8c2c67055a0e612904
sonya <s44643685@gmail.com>
2026-04-25 11:29:23 +0200

    start progect

--- 4.py
+++ 4.py
@@ -1,4 +1,6 @@
 # ? boolean / bool / логические тип
+from math import ceil
+
 bool_1 = True
 print(bool_1, type(bool_1))
 bool_2 = False
@@ -75,4 +77,58 @@ else:
 # not - нет
 # and - и
 # or - или
+from math import*
+# # дз 7 1165
+#  k = 2
+#  V = 18
+#  n = 1.5
+#  i = 1 / 6
+
+# # дз 11 3889
+# L = 115
+# N = 10 + 1020
+# i = ceil(log2(N))
+# I = L * i / 8
+# V = I * 16384 / 2**10
+# print(V)
+
+# # 11
+# N = 27 + 10
+# i = ceil(log2(N))
+# n = 3548
+# V = 12
+# I = ceil(V * 2**10 / n)
+# print(I)
+# L = I * 8 / i
+# print(L)
+# print(ceil(floor(L) * i / 8))
+# print(ceil(ceil(L) * i / 8))
+
+# # 11
+# L = 172
+# V = 54
+# n = 356984
+# I = ceil(V * 2**20 / n)
+# print("I", I)
+# i = I * 8 / L
+# print("i" , i)
+# print(ceil(L * floor(i) / 8))
+# print(ceil(L * ceil(i) / 8))
+
+# # 7
+# k = 2
+# n = 48 * 1000
+# p = 13
+# t = 42 * 60 + 20
+# i = 34
+# V = k * n * t * i + p * 110 * 2**13
+# q = 314572800
+# t_q = V / q
+# print(t_q)
+
+V1 = 2560 * 1440 * 22
+V2 = 1920 * 1080 * 20
+print((V1 - V2) * 130 / 2**13)
+
+
 

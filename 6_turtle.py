a25a02db481c07ad873fc717f515c03ebdf0b8e8
sonya <s44643685@gmail.com>
2026-05-02 11:30:28 +0200

    start progect

---
+++ 6_turtle.py
@@ -0,0 +1,42 @@
+from turtle import *
+screensize(2000,2000) #увеличение экрана
+tracer(False) #отключает анимацию
+# forward(100) #движение вперед
+# back(50) #движение назад
+# left(90) #движение влево
+# right(90) #движение вправо
+# up() #поднять перо
+# down() #опустить перо
+m = 20
+left(90)
+
+for i in range(2):
+    fd(10 * m)
+    rt(90)
+    fd(18 * m)
+    rt(90)
+
+up()
+
+fd(5 * m)
+rt(90)
+fd(7 * m)
+lt(90)
+
+down()
+
+for i in range(2):
+    fd(10 * m)
+    rt(90)
+    fd(7 * m)
+    rt(90)
+
+up()
+for x in range(7, 15):
+    for y in range(5, 11):
+        goto(x * m, y * m)
+        dot(3,"red")
+print(19 * 11 + (15 - 7) * (16 - 8) - (15 - 7) * (11 - 5))
+
+update() #обновляет экран
+done() #рисунок остается на экране
